daf297a9ae82cc61cf805fab568f18341301a977
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2025-10-15 18:13:31 +0100

    Added basic Damage but need to convert Moves into a hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Random;
+
 public class Main {
+    public static void Damage(Pokemon Pokemon1, Pokemon Pokemon2, int Player1Move, int Player2Move) {
+        if (Player1Move == 4 || Player2Move == 4) {
+            Random dice = new Random();
+            float result = dice.nextFloat();
+            if (result < 0.8f) {
+                return;
+            }
+        }
+
+        // TODO: Convert Moves into a hashmap, eg: "Growl": 10, 10 being damage value
+        Pokemon1.setHealth(Pokemon1.getHealth() - (
+                Pokemon2.Moves.values().get(Player2Move)
+                        * Pokemon2.effectiveness.get(Pokemon1.Type)));
+    }
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Welcome to Pokemon!");
         Charmander playerPokemon = new Charmander();
-        playerPokemon.setHealth(playerPokemon.getHealth() - 15);
-        System.out.println("Player's Charmander health is: " + playerPokemon.getHealth());
+        Bulbasaur enemyPokemon = new Bulbasaur();
     }
 }
