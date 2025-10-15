486953ffe91dda54e8b4da491634272ad538192e
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2025-10-15 16:53:56 +0100

    Added all 4 pokemons

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -8,7 +8,6 @@ public class Pokemon {
     public Pokemon() {
         Health = 100;
         Type = "";
-
     }
 
     public int getHealth() {
@@ -26,7 +25,41 @@ class Charmander extends Pokemon {
 
         effectiveness.put("fire", 1.0f);
         effectiveness.put("water", 0.75f);
-        effectiveness.put("lightning", 0.75f);
+        effectiveness.put("electric", 0.75f);
         effectiveness.put("grass", 2.0f);
     }
 }
+
+class Pikachu extends Pokemon {
+    public Pikachu() {
+        Type = "electric";
+
+        effectiveness.put("fire", 1.0f);
+        effectiveness.put("water", 2.0f);
+        effectiveness.put("electric", 1.0f);
+        effectiveness.put("grass", 0.75f);
+    }
+}
+
+class Squirtle extends Pokemon {
+    public Squirtle() {
+        Type = "water";
+
+        effectiveness.put("fire", 2.0f);
+        effectiveness.put("water", 1.0f);
+        effectiveness.put("electric", 1.0f);
+        effectiveness.put("grass", 0.75f);
+    }
+}
+
+class Bulbasaur extends Pokemon {
+    public Bulbasaur() {
+        Type = "grass";
+
+        effectiveness.put("fire", 0.75f);
+        effectiveness.put("water", 2.0f);
+        effectiveness.put("electric", 0.75f);
+        effectiveness.put("grass", 1.0f);
+    }
+}
+
